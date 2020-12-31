914d47096af932657b0d052edc0ca7bc03c53ec2
Yogita Mahele <mahaleyogita233@gmail.com>
2020-12-31 18:07:43 +0530

    add referfriendSlider table

---
+++ plathora.Entity/referfriendSlider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace plathora.Entity
+{
+  public class referfriendSlider
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        [DefaultValue("false")]
+        public Boolean isdeleted { get; set; }
+        [DefaultValue("false")]
+        public Boolean isactive { get; set; }
+    }
+}
